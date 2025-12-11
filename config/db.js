202051d3ad9ee366ef1933cf620de5cf0c42ba15
Abu Asad MD. Tasin <tasinasad@gmail.com>
2025-12-11 18:56:55 +0600

    Fix User schema and DB connection

--- config/db.js
+++ config/db.js
@@ -7,18 +7,11 @@ if (!cached) {
 }
 
 async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose
-      .connect(`${process.env.MONGODB_URI}/quickcart`, opts)
-      .then((mongoose) => mongoose);
+    const opts = { bufferCommands: false };
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
--- models/User.js
+++ models/User.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
+  _id: { type: String, required: true },   // Clerk ID
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   imageUrl: { type: String, required: true },
   cartItems: { type: Object, default: {} }
-}, { minimize: false })
+}, { minimize: false });
 
-const User = mongoose.models.user || mongoose.model('user', userSchema)
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
-export default User
+export default User;
