97703baaaafd70ff7e1643cc77d7f30ec7033518
Abu Asad MD. Tasin <tasinasad@gmail.com>
2025-12-11 17:55:46 +0600

    Fix case-sensitive import for User model

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
-import User from "@/models/user";
+import User from "@/models/User";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
