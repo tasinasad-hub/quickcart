c1e80714722b73a0a09a04242823f45f675d5bf8
Abu Asad MD. Tasin <tasinasad@gmail.com>
2025-12-15 22:38:18 +0600

    Restore Order.js with correct case

---
+++ models/Order.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  userId: { type: String, ref: 'user', required: true }, // Clerk ID string
+  items: [
+    {
+      product: { type: mongoose.Schema.Types.ObjectId, ref: 'product', required: true },
+      quantity: { type: Number, required: true }
+    }
+  ],
+  amount: { type: Number, required: true },
+  address: { type: mongoose.Schema.Types.ObjectId, ref: 'address', required: true },
+  status: { type: String, required: true, default: "Order Placed" },
+  date: { type: Number, required: true }
+});
+
+const Order = mongoose.models.order || mongoose.model('order', orderSchema);
+
+export default Order;
